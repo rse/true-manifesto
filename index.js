324327b1e4c65b38f378674553c793d99835d141
Ralf S. Engelschall <rse@engelschall.com>
2019-05-25 12:08:24 +0200

    add About page and restructure content

--- index.js
+++ index.js
@@ -5,17 +5,45 @@
 */
 
 $(document).ready(function () {
-    var selectLang = function (lang) {
+    var page = "manifesto"
+    var lang = "en"
+    var updateHash = function () {
+        var hash = "#" + page + "-" + lang
+        if (window.location.hash !== hash)
+            window.location.hash = hash
+    }
+    var selectPage = function () {
+        $(".content > .page").css("display", "none")
+        $(".content > .page.page-" + page).css("display", "block")
+        $(".header .page").removeClass("active")
+        $(".header .page-" + page).addClass("active")
+        updateHash()
+    }
+    var selectLang = function () {
         $("*[lang]").css("display", "none")
         $("*[lang='" + lang + "']").css("display", "block")
         $("span[lang='" + lang + "']").css("display", "inline")
-        $(".icon-lang").removeClass("active")
-        $("#icon-lang-" + lang).addClass("active")
+        $(".header .icon-lang").removeClass("active")
+        $(".header .icon-lang-" + lang).addClass("active")
+        updateHash()
     }
-    $(".langs .icon-lang").click(function (ev) {
-        var lang = $(ev.target).attr("id").replace(/^icon-lang-/, "")
-        selectLang(lang)
+    $(".header a").click(function (ev) {
+        ev.preventDefault()
     })
-    selectLang("en")
+    $(".header .pages .page").click(function (ev) {
+        page = $(ev.target).attr("class").replace(/^.*page-([a-z]+).*$/, "$1")
+        selectPage()
+    })
+    $(".header .langs .icon-lang").click(function (ev) {
+        lang = $(ev.target).attr("class").replace(/^.*icon-lang-([a-z]+).*$/, "$1")
+        selectLang()
+    })
+    var m
+    if ((m = window.location.hash.match(/^#(manifesto|tenets|about)-(en|de)$/)) !== null) {
+        page = m[1]
+        lang = m[2]
+    }
+    selectPage(page)
+    selectLang(lang)
 })
 
